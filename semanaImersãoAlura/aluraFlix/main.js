d1ae9234ddd7fc3ca71461ccd5298ca5c18baab0
Ana Carolina <101834138+ananazario@users.noreply.github.com>
2022-09-12 16:39:26 -0300

    Add files via upload

--- semanaImersãoAlura/aluraFlix/main.js
+++ semanaImersãoAlura/aluraFlix/main.js
@@ -1,6 +1,15 @@
-var listaFilmes = ["https://br.web.img3.acsta.net/medias/nmedia/18/92/91/32/20224832.jpg", "https://upload.wikimedia.org/wikipedia/pt/5/59/The_Lord_of_the_Rings_The_Two_Towers.jpg", "https://upload.wikimedia.org/wikipedia/pt/6/6c/Tarzan_filme_2013_.jpg", "https://upload.wikimedia.org/wikipedia/pt/9/9f/Thor_Poster.jpg", "https://upload.wikimedia.org/wikipedia/pt/b/b3/Thor_The_Dark_World_2013.jpg", "https://upload.wikimedia.org/wikipedia/pt/e/e6/Pirates_of_the_Caribbean_-_The_Curse_of_the_Black_Pearl.jpg"];
-
-for (var indice = 0; indice < listaFilmes.length; indice++) {
-    document.write("<img src=" + listaFilmes[indice] +">")
+function adicionarFilme() {
+    var filmeFavorito = document.querySelector("#filme").value
+    if (filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito)
+    } else {
+        console.error("Endereço de filme inválido")
+    }
+    document.querySelector("#filme").value = ""
 }
 
+function listarFilmesNaTela(filme) {
+    var elementoFilmeFavorito = "<img src=" + filme + ">"
+    var elementoListaFilmes = document.querySelector("#listaFilmes")
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito
+}
--- semanaImersãoAlura/mentalista/main.js
+++ semanaImersãoAlura/mentalista/main.js
@@ -3,10 +3,14 @@ var numeroSecreto = parseInt(Math.random() * 11)
 function Chutar() {
     var chute = parseInt(document.querySelector("#valor").value)
     var elementoResultado = document.querySelector("#resultado")
+    const score = document.querySelector(".score")
+    let scoreCount = 0
     
-    if(chute == numeroSecreto) {
-        elementoResultado.innerHTML = "Você acertou!"    
-    } else if(chute > 10 || chute < 0) {
+    if (chute == numeroSecreto) {
+        elementoResultado.innerHTML = "Você acertou!" 
+        scoreCount = scoreCount + 1
+        score.innerText = `score: ${scoreCount}`
+    } else if (chute > 10 || chute < 0) {
         elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10!"    
     } else {
         elementoResultado.innerHTML = `Errou, o número secreto era ${numeroSecreto}!`   
